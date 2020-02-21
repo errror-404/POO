9dd9285fad107729f23dfc8198a3eeb7f716d5a8
errror-404 <60365507+errror-404@users.noreply.github.com>
2020-02-21 12:26:16 -0800

    tercera version

--- Peliculas/Peliculas}/Program.cs
+++ Peliculas/Peliculas}/Program.cs
@@ -7,22 +7,14 @@ public class Pelicula{
 	private string pais;
 	private string director;
 	
-	public void SetTitulo(string n){
-		titulo = n;	
+	public Pelicula(String p, int a ){
+		titulo = p;
+		año = a;
 	}
 	
-	public string GetTitulo(){
-		return titulo;	
+	public void imprime(){
+		Console.WriteLine(titulo + "\t" + año);	
 	}
-	
-	public void SetAño(int y){
-		año = y;	
-	}
-	
-	public int GetAño(){
-		return año;	
-	}
-	
 }
 
 
@@ -31,15 +23,8 @@ public class Program
 	
 	public static void Main()
 	{
-		Pelicula p1 = new Pelicula();
-		p1.SetTitulo("Parasitos");
-		p1.SetAño(2019);
-		
-		Pelicula p2 = new Pelicula();
-		p2.SetTitulo("La La Land");
-		p2.SetAño(2016);
-		
-		Console.WriteLine(p1.GetTitulo() + "\t" + p1.GetAño() + "\n" + p2.GetTitulo() + "\t" + p2.GetAño());
+		Pelicula p1 = new Pelicula("Parasitos",2019);
+		p1.imprime();
 		
 	}
 }
