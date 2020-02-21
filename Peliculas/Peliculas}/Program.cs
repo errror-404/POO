a6db8e8ed25326a9efeb2ef424835ea8fb68d298
errror-404 <60365507+errror-404@users.noreply.github.com>
2020-02-21 12:27:19 -0800

    version final

--- Peliculas/Peliculas}/Program.cs
+++ Peliculas/Peliculas}/Program.cs
@@ -1,30 +1,36 @@
 ﻿using System;
-			
-public class Pelicula{
+using System.Collections.Generic;	
 
+
+public class Pelicula{
 	private string titulo;
 	private int año;
 	private string pais;
 	private string director;
 	
-	public Pelicula(String p, int a ){
-		titulo = p;
+	public Pelicula(string t, int a){
+		titulo = t;
 		año = a;
 	}
-	
 	public void imprime(){
 		Console.WriteLine(titulo + "\t" + año);	
 	}
 }
 
-
 public class Program
 {
-	
 	public static void Main()
 	{
-		Pelicula p1 = new Pelicula("Parasitos",2019);
-		p1.imprime();
+	
+		List<Pelicula> P1 = new List<Pelicula>();
+		P1.Add(new Pelicula ("Toy Story 3", 2019));
+		P1.Add(new Pelicula ("La La Land",2016));
+		P1.Add(new Pelicula ("Parasitos",2019));
+		P1.Add(new Pelicula ("Joker",2019));
+		P1.Add(new Pelicula ("Krauss",2019));
 		
+		foreach (Pelicula p in P1){
+			p.imprime();
+		}
 	}
 }
