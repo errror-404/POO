98a0efda66134cac4f5d33b15c14383f398967df
errror-404 <60365507+errror-404@users.noreply.github.com>
2020-02-21 12:40:46 -0800

    version final mejorada

--- Peliculas/Peliculas}/Program.cs
+++ Peliculas/Peliculas}/Program.cs
@@ -8,12 +8,15 @@ public class Pelicula{
 	private string pais;
 	private string director;
 	
-	public Pelicula(string t, int a){
+	public Pelicula(string t, int a, string p, string d ){
 		titulo = t;
 		año = a;
+        pais = p;
+        director = d;
 	}
+
 	public void imprime(){
-		Console.WriteLine(titulo + "\t" + año);	
+		Console.WriteLine(titulo + "\t" + año+ "\t" + pais+ "\t" + director);	
 	}
 }
 
@@ -23,11 +26,11 @@ public class Program
 	{
 	
 		List<Pelicula> P1 = new List<Pelicula>();
-		P1.Add(new Pelicula ("Toy Story 3", 2019));
-		P1.Add(new Pelicula ("La La Land",2016));
-		P1.Add(new Pelicula ("Parasitos",2019));
-		P1.Add(new Pelicula ("Joker",2019));
-		P1.Add(new Pelicula ("Krauss",2019));
+		P1.Add(new Pelicula ("Toy Story 3", 2019,"USA","PIXART"));
+		P1.Add(new Pelicula ("La La Land",2016,"USA","Damien Chazelle"));
+		P1.Add(new Pelicula ("Parasitos",2019,"KOREA","Bong Joon-ho"));
+		P1.Add(new Pelicula ("Joker",2019,"USA","Todd Phillips"));
+		P1.Add(new Pelicula ("Klaus",2019,"ESPAÑA","Sergio Pablos"));
 		
 		foreach (Pelicula p in P1){
 			p.imprime();
