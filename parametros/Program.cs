40e58f73b9a18825358e488555c4c025239f4418
errror-404 <60365507+errror-404@users.noreply.github.com>
2020-03-13 19:35:07 -0800

    pase de parametro por valor y referencia

---
+++ parametros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace parametros
+{
+
+    class avicci{
+
+        public static void suma(ref int a){
+
+            a++;
+
+        }
+
+        public static void resta(in int a, in int b, out int c){
+            c = a - b;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num = 7;int num2 = 5; int res = 0;
+           avicci.suma(ref num);
+           avicci.resta(in num, in num2, out res);
+           Console.WriteLine(num);
+           Console.WriteLine(res);
+        }
+    }
+}
